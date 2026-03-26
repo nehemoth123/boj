b73cc5a856ccc736bf8f1f1e03c73655fe307291
nehemoth123 <aafgrti@gmail.com>
2026-03-26 14:12:32 +0900

    [Silver IV] Title: 큐, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -4,22 +4,22 @@ que=[]
 for i in range(n):
     command=list(sys.stdin.readline().split())
     if command[0] == "push":
-        que.insert(0,command[1])
+        que.append(command[1])
     elif command[0] == "front":
         if len(que) == 0:
             print(-1)
         else:
-            print(que[len(que)-1])
+            print(que[0])
     elif command[0]=="pop":
         if len(que) == 0:
             print(-1)
         else:
-            print(que.pop(len(que)-1))
+            print(que.pop(0))
     elif command[0]=="back":
         if len(que) == 0:
             print(-1)
         else:
-            print(que[0])
+            print(que[len(que)-1])
     elif command[0] == "empty":
         if len(que) == 0:
             print(1)
